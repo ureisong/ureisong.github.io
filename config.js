92bdaf9774ddca90a0fe1f199f1a611f15716556
ureisong <283483822+ureisong@users.noreply.github.com>
2026-05-11 12:18:46 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+window.APP_CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbx8bBL_2PqVj5XbK7KhUjyuxG3l8nwkqkJWuLzDS3LVOr5v_-1v-y_X9CoBYHylijmhjg/exec",
+  DATA_URL: "./data.json",
+  SONGS_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTznVKN0u7Eei7Dz4wvlDYaHd_PR-a-2taqdzO1nP5TMLrWTccvjXPLgUKeArHuVl00SlJNsqV36p0n/pub?gid=1754859151&single=true&output=csv",
+  LIKES_SMR_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTznVKN0u7Eei7Dz4wvlDYaHd_PR-a-2taqdzO1nP5TMLrWTccvjXPLgUKeArHuVl00SlJNsqV36p0n/pub?gid=2053464052&single=true&output=csv",
+  NOTICE_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTznVKN0u7Eei7Dz4wvlDYaHd_PR-a-2taqdzO1nP5TMLrWTccvjXPLgUKeArHuVl00SlJNsqV36p0n/pub?gid=872049504&single=true&output=csv",
+  SETTINGS_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTznVKN0u7Eei7Dz4wvlDYaHd_PR-a-2taqdzO1nP5TMLrWTccvjXPLgUKeArHuVl00SlJNsqV36p0n/pub?gid=1051238357&single=true&output=csv",
+  SEARCH_ALIASES_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTznVKN0u7Eei7Dz4wvlDYaHd_PR-a-2taqdzO1nP5TMLrWTccvjXPLgUKeArHuVl00SlJNsqV36p0n/pub?gid=1311743505&single=true&output=csv",
+  LIKE_COOLDOWN_MS: 60 * 1000,
+  NOTICE_TEXT: "",
+  FOOTER_TEXT: ""
+};
