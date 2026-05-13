651c15c45c178178b3de5fbf7b39dffba10c4dcc
ureisong <283483822+ureisong@users.noreply.github.com>
2026-05-13 10:49:56 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyymIj87KFv-oeYIGbaadgO6wC6j2oG_aLEpTp5Vva-V2bYBHiQRZ9tbFZkqofLx3MmKQ/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbyMvt0VjgrF6kFboImWAT5wqdXKREUKIIGMq72yEQi8270OUs4re8gnoHDg3-Hff5W_nA/exec",
   DATA_URL: "./data.json",
   SONGS_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTznVKN0u7Eei7Dz4wvlDYaHd_PR-a-2taqdzO1nP5TMLrWTccvjXPLgUKeArHuVl00SlJNsqV36p0n/pub?gid=1754859151&single=true&output=csv",
   LIKES_SMR_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTznVKN0u7Eei7Dz4wvlDYaHd_PR-a-2taqdzO1nP5TMLrWTccvjXPLgUKeArHuVl00SlJNsqV36p0n/pub?gid=2053464052&single=true&output=csv",
